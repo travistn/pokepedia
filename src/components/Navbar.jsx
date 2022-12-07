71b7a592755acf8ee1d3a3004bdb788003e5713d
travistn <travisn714@gmail.com>
2022-12-06 17:31:49 -0800

    Edited navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row justify-center items-center w-screen h-[50px] bg-gray-300'>
+    <div className='flex flex-row justify-center items-center h-[50px] bg-gray-300'>
       <h5 className='text-[20px]'>PokéPedia</h5>
     </div>
   );
