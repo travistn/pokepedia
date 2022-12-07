d84ad97288f2d7af0d4190bcc302080de5716713
travistn <travisn714@gmail.com>
2022-12-06 19:51:44 -0800

    Added styling for desktop view

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -4,7 +4,7 @@ const TypeCard = ({ type }) => {
   return (
     <div
       id={type?.name}
-      className={`w-[66px] h-[26px] flex justify-center items-center rounded-[4px] text-[12px] uppercase bg-${type?.name} text-white`}>
+      className={`w-[66px] lg:w-[84px] h-[26px] lg:h-[34px] flex justify-center items-center rounded-[4px] text-[12px] lg:text-[13px] uppercase bg-${type?.name} text-white`}>
       {type?.name}
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,11 +12,14 @@ const Home = () => {
 
   return (
     <div className='flex flex-col items-center'>
-      <div className='flex flex-col mt-8 w-11/12 items-center'>
-        <div className='flex flex-row flex-wrap gap-1 flex-1 justify-center'>
-          {types?.map((type) => (
-            <TypeCard type={type} key={type?.name} />
-          ))}
+      <div className='flex flex-col mt-8 w-11/12 items-center gap-8'>
+        <div className='flex flex-col items-center gap-4 lg:w-[45%]'>
+          <h1 className='lg:text-[20px]'>Pokémon Types</h1>
+          <div className='flex flex-row flex-wrap gap-1 flex-1 justify-center'>
+            {types?.map((type) => (
+              <TypeCard type={type} key={type?.name} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
