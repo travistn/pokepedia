f6c80c4284caef28fd5853779c1e11773205b69c
travistn <travisn714@gmail.com>
2022-12-07 13:40:56 -0800

    Added styling for type card

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -4,7 +4,7 @@ const TypeCard = ({ type }) => {
   return (
     <div
       id={type?.name}
-      className={`w-[66px] lg:w-[84px] h-[26px] lg:h-[34px] flex justify-center items-center rounded-[4px] text-[12px] lg:text-[13px] uppercase bg-${type?.name} text-white`}>
+      className={`w-[66px] lg:w-[84px] h-[26px] lg:h-[34px] flex justify-center items-center rounded-[4px] text-[12px] lg:text-[13px] uppercase bg-${type?.name} text-white cursor-pointer hover:opacity-80`}>
       {type?.name}
     </div>
   );
