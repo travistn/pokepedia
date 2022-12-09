4d4a792f145aa0a4f094cd69675a391793f21b0c
travistn <travisn714@gmail.com>
2022-12-09 15:14:48 -0800

    Clicking on navbar will redirect to home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import spheal from '../assets/spheal.png';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='flex flex-row justify-center items-center h-[70px] lg:h-[90px] bg-[#0d293a]'>
-      <h5 className='text-[20px] lg:text-[26px] text-white'>PokéPedia</h5>
-      <img src={spheal} className='w-[70px] lg:w-[90px] h-[70px] lg:h-[90px] ml-2' />
+      <h5
+        className='text-[20px] lg:text-[26px] text-white cursor-pointer'
+        onClick={() => navigate('/')}>
+        PokéPedia
+      </h5>
+      <img
+        src={spheal}
+        className='w-[70px] lg:w-[90px] h-[70px] lg:h-[90px] ml-2 cursor-pointer'
+        onClick={() => navigate('/')}
+      />
     </div>
   );
 };
