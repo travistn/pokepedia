0341424d61cebabcaee2cb6c3f9183e7f55171fa
travistn <travisn714@gmail.com>
2022-12-08 19:34:17 -0800

    Added styling for desktop view

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,9 +5,14 @@ import TypeCard from './TypeCard';
 const PokemonCard = ({ pokemon }) => {
   return (
     <div className='flex flex-col items-center'>
-      <img src={pokemon?.sprites.other['official-artwork'].front_default} className='h-[170px]' />
-      <p className='text-[14px] opacity-70'>{`#${pokemon?.id}`}</p>
-      <h4 className='capitalize text-blue-600 font-bold text-[18px]'>{pokemon?.name}</h4>
+      <img
+        src={pokemon?.sprites.other['official-artwork'].front_default}
+        className='h-[170px] lg:h-[220px]'
+      />
+      <p className='text-[14px] lg:text-[15px] opacity-70'>{`#${pokemon?.id}`}</p>
+      <h4 className='capitalize text-blue-600 font-bold text-[18px] lg:text-[22px]'>
+        {pokemon?.name}
+      </h4>
       <div className='flex flex-row gap-1 mt-1'>
         {pokemon?.types.map((type) => (
           <TypeCard type={type?.type} key={type?.type.name} />
