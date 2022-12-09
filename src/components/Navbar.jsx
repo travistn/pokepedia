1087a27f51714cb5aa917f14415bfe3cf0e87d83
travistn <travisn714@gmail.com>
2022-12-08 19:28:53 -0800

    Edited navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import spheal from '../../assets/spheal.png';
+import spheal from '../assets/spheal.png';
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row justify-center items-center h-[70px] lg:h-[90px] bg-[#E3F2FD]'>
-      <h5 className='text-[20px] lg:text-[26px]'>PokéPedia</h5>
+    <div className='flex flex-row justify-center items-center h-[70px] lg:h-[90px] bg-[#0d293a]'>
+      <h5 className='text-[20px] lg:text-[26px] text-white'>PokéPedia</h5>
       <img src={spheal} className='w-[70px] lg:w-[90px] h-[70px] lg:h-[90px] ml-2' />
     </div>
   );
