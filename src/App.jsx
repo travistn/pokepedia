7210595f7011ac71e850b1fb2ea699248e0edea8
travistn <travisn714@gmail.com>
2022-12-12 15:09:11 -0800

    Added a pokemon page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Pokemon from './pages/Pokemon';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/pokemon/:pokeId' element={<Pokemon />} />
       </Routes>
     </>
   );
