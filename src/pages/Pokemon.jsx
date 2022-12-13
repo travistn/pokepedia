51d90081a20ca21e7429f54871e5ce7986b11500
travistn <travisn714@gmail.com>
2022-12-12 18:20:52 -0800

    Created pokemon page

---
+++ src/pages/Pokemon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Pokemon = () => {
+  return (
+    <div className='h-screen flex flex-col items-center bg-[#d2e1f2] lg:bg-poke-bg bg-no-repeat bg-contain'></div>
+  );
+};
+
+export default Pokemon;
