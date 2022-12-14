050197bb3b4ebedbc5a98da43b0926e8aa64c0e7
travistn <travisn714@gmail.com>
2022-12-13 20:27:41 -0800

    Added pokemon's abilities

--- src/components/PokedexData.jsx
+++ src/components/PokedexData.jsx
@@ -5,6 +5,8 @@ import { decimeterToFeet } from '../reuseables/decimeterToFeet';
 import { hectogramsToPounds } from '../reuseables/hectogramsToPounds';
 
 const PokedexData = ({ pokemon, species }) => {
+  console.log(pokemon);
+
   return (
     <div className='flex flex-col gap-2'>
       <h1 className='text-center text-[20px] font-bold'>Pokédex Data</h1>
@@ -20,6 +22,17 @@ const PokedexData = ({ pokemon, species }) => {
       <p>Species: {species?.genera.find((item) => item.language.name === 'en').genus}</p>
       <p>Height: {decimeterToFeet(pokemon?.height)} ft.</p>
       <p>Weight: {hectogramsToPounds(pokemon?.weight)} lbs.</p>
+      <div className='flex flex-row items-center'>
+        <p>Abilities:</p>
+        <div className='flex flex-col ml-2'>
+          {pokemon?.abilities.map((ability, index) => (
+            <p key={index} className='capitalize'>
+              {ability?.ability.name.split('-').join(' ') +
+                (ability?.is_hidden === true ? '(hidden ability)' : '')}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
