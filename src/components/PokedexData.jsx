04c54b4c5063d8e71f07d256010a53b016a7023a
travistn <travisn714@gmail.com>
2022-12-13 22:12:12 -0800

    Added more pokedex data

--- src/components/PokedexData.jsx
+++ src/components/PokedexData.jsx
@@ -5,25 +5,47 @@ import { decimeterToFeet } from '../reuseables/decimeterToFeet';
 import { hectogramsToPounds } from '../reuseables/hectogramsToPounds';
 
 const PokedexData = ({ pokemon, species }) => {
-  console.log(pokemon);
+  const pokemonGenderRatio = (genderRate) => {
+    if (genderRate !== -1) {
+      return (
+        <div className='flex flex-row gap-1 font-normal'>
+          <p className='text-blue-600'>{((8 - genderRate) / 8) * 100}% male,</p>
+          <p className='text-pink-500'>{(genderRate / 8) * 100}% female</p>
+        </div>
+      );
+    } else {
+      return 'Genderless';
+    }
+  };
 
   return (
     <div className='flex flex-col gap-2'>
       <h1 className='text-center text-[20px] font-bold'>Pokédex Data</h1>
-      <p>
-        National No.<span className='font-bold'> {pokemon?.id}</span>
+      <p className='font-bold'>
+        National No.<span className='font-normal'> {pokemon?.id}</span>
       </p>
       <div className='flex flex-row gap-1 items-center'>
-        Type:
+        <p className='font-bold'>Type:</p>
         {pokemon?.types.map((type) => (
           <TypeCard type={type.type} key={type?.type.name} />
         ))}
       </div>
-      <p>Species: {species?.genera.find((item) => item.language.name === 'en').genus}</p>
-      <p>Height: {decimeterToFeet(pokemon?.height)} ft.</p>
-      <p>Weight: {hectogramsToPounds(pokemon?.weight)} lbs.</p>
+      <p className='flex flex-row gap-1 font-bold'>
+        Species:
+        <span className='font-normal'>
+          {species?.genera.find((item) => item.language.name === 'en').genus}
+        </span>
+      </p>
+      <p className='flex flex-row gap-1 font-bold'>
+        Height:
+        <span className='font-normal'>{decimeterToFeet(pokemon?.height)} ft.</span>
+      </p>
+      <p className='flex flex-row gap-1 font-bold'>
+        Weight:
+        <span className='font-normal'>{hectogramsToPounds(pokemon?.weight)} lbs.</span>
+      </p>
       <div className='flex flex-row items-center'>
-        <p>Abilities:</p>
+        <p className='font-bold'>Abilities:</p>
         <div className='flex flex-col ml-2'>
           {pokemon?.abilities.map((ability, index) => (
             <p key={index} className='capitalize'>
@@ -32,6 +54,15 @@ const PokedexData = ({ pokemon, species }) => {
             </p>
           ))}
         </div>
+      </div>
+      <p className='flex flex-row gap-1 font-bold'>
+        Capture rate:
+        <span className='font-normal'>{species?.capture_rate}</span>
+      </p>
+      <div className='flex flex-row'>
+        <p className='flex flex-row gap-1 font-bold'>
+          Gender: {pokemonGenderRatio(species?.gender_rate)}
+        </p>
       </div>
     </div>
   );
