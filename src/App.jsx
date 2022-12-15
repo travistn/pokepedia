f6c2782708ab2cdd364a60aa2d35aa4513d9aa22
travistn <travisn714@gmail.com>
2022-12-15 00:49:10 -0800

    Created pokedex directory page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Pokemon from './pages/Pokemon';
+import PokedexDirectory from './pages/PokedexDirectory';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/pokemon/:pokeId' element={<Pokemon />} />
+        <Route path='/pokedex' element={<PokedexDirectory />} />
       </Routes>
     </>
   );
---
+++ src/pages/PokedexDirectory.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PokedexDirectory = () => {
+  return <div></div>;
+};
+
+export default PokedexDirectory;
