9ba998572f30d42d125169b20d3a07128b633f0c
travistn <travisn714@gmail.com>
2022-12-14 17:46:31 -0800

    first commit

---
+++ src/reuseables/getPokemonGenderRatio.jsx
@@ -0,0 +1,13 @@
+export const pokemonGenderRatio = (genderRate) => {
+  if (genderRate !== -1) {
+    return (
+      <div className='flex flex-row font-normal'>
+        <p className='text-blue-600'>{((8 - genderRate) / 8) * 100}% male</p>
+        <span>{`, `}</span>
+        <p className='text-pink-500 ml-1'>{(genderRate / 8) * 100}% female</p>
+      </div>
+    );
+  } else {
+    return 'Genderless';
+  }
+};
