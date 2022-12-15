51c0c16e51c920ece510b3c6289b5fd1e4419edf
travistn <travisn714@gmail.com>
2022-12-15 14:53:15 -0800

    Added links to go to a specific generation pokedex

--- src/pages/PokedexDirectory.jsx
+++ src/pages/PokedexDirectory.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PokedexDirectory = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
       <div className='flex flex-col items-center w-10/12 mt-8 gap-8'>
-        <h1 className='text-[20px] font-bold bg-white w-full text-center rounded-md pt-2 pb-2'>
-          Pokémon Pokédex
-        </h1>
-        <p className='bg-white p-4 w-full rounded-md'>
-          The Pokédex section has a wealth of information on all the Pokémon creatures from the
-          entire game series. On the main list pages you can see the various stats of each Pokémon.
-          Click a Pokémon's name to see a detailed page with Pokédex data.
-        </p>
+        <div className='w-full flex flex-col bg-white rounded-md p-4'>
+          <h1 className='text-[22px] font-bold text-center'>Pokémon Pokédex</h1>
+          <p className='mt-4'>
+            The Pokédex section has a wealth of information on all the Pokémon creatures from the
+            entire game series. On the main list pages you can see the various stats of each
+            Pokémon. Click a Pokémon's name to see a detailed page with Pokédex data.
+          </p>
+        </div>
+        <div className='w-full bg-white rounded-md flex flex-col p-4 gap-4'>
+          <h1 className='font-bold text-[20px] text-center'>Native Pokédexes</h1>
+          <p>
+            Each game has a native Pokédex with the Pokémon from that region. The national dex lists
+            all Pokémon in their original order.
+          </p>
+          <div className='flex flex-col gap-2'>
+            <p className='font-bold' onClick={() => navigate('/pokedex/kanto')}>
+              Generation I <span className='text-[15px] font-normal opacity-70'>{'(Kanto)'}</span>
+            </p>
+            <p className='font-bold' onClick={() => navigate('/pokedex/johto')}>
+              Generation II <span className='text-[15px] font-normal opacity-70'>{'(Johto)'}</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
