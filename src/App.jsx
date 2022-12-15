f9aaa131c9ee35430bf237082dcfcd7179ef803c
travistn <travisn714@gmail.com>
2022-12-15 14:44:22 -0800

    Created pokedex page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Pokemon from './pages/Pokemon';
 import PokedexDirectory from './pages/PokedexDirectory';
+import Pokedex from './pages/Pokedex';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/pokemon/:pokeId' element={<Pokemon />} />
         <Route path='/pokedex' element={<PokedexDirectory />} />
+        <Route path='/pokedex/:region' element={<Pokedex />} />
       </Routes>
     </>
   );
