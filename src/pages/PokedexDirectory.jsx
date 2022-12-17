c05d00554cf3893035d6e69943cca8b3fcd0bff6
travistn <travisn714@gmail.com>
2022-12-16 16:23:52 -0800

    Added more regions for the pokedex

--- src/pages/PokedexDirectory.jsx
+++ src/pages/PokedexDirectory.jsx
@@ -8,25 +8,61 @@ const PokedexDirectory = () => {
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
       <div className='flex flex-col items-center w-10/12 mt-8 gap-8 lg:w-7/12 lg:bg-dark-light-blue rounded-md lg:p-8'>
         <div className='w-full flex flex-col bg-white rounded-md p-4'>
-          <h1 className='text-[22px] font-bold text-center'>Pokémon Pokédex</h1>
+          <h1 className='text-[22px] lg:text-[26px] font-bold text-center'>Pokémon Pokédex</h1>
           <p className='mt-4'>
             The Pokédex section has a wealth of information on all the Pokémon creatures from the
             entire game series. On the main list pages you can see the various stats of each
             Pokémon. Click a Pokémon's name to see a detailed page with Pokédex data.
           </p>
         </div>
-        <div className='w-full bg-white rounded-md flex flex-col p-4 gap-4'>
-          <h1 className='font-bold text-[20px] text-center'>Native Pokédexes</h1>
+        <div className='w-full bg-white rounded-md flex flex-col p-4 gap-4 lg:w-[60%]'>
+          <h1 className='font-bold text-[20px] lg:text-[24px] text-center'>Native Pokédexes</h1>
           <p>
             Each game has a native Pokédex with the Pokémon from that region. The national dex lists
             all Pokémon in their original order.
           </p>
           <div className='flex flex-col gap-2'>
-            <p className='font-bold' onClick={() => navigate('/pokedex/kanto')}>
-              Generation I <span className='text-[15px] font-normal opacity-70'>{'(Kanto)'}</span>
+            <p
+              className='font-bold hover:cursor-pointer hover:underline lg:text-[18px]'
+              onClick={() => navigate('/pokedex/kanto')}>
+              Generation I{' '}
+              <span className='text-[15px] lg:-[16px] font-normal opacity-70'>{'(Kanto)'}</span>
             </p>
-            <p className='font-bold' onClick={() => navigate('/pokedex/johto')}>
-              Generation II <span className='text-[15px] font-normal opacity-70'>{'(Johto)'}</span>
+            <p
+              className='font-bold hover:cursor-pointer hover:underline lg:text-[18px]'
+              onClick={() => navigate('/pokedex/johto')}>
+              Generation II{' '}
+              <span className='text-[15px] lg:-[16px] font-normal opacity-70'>{'(Johto)'}</span>
+            </p>
+            <p
+              className='font-bold hover:cursor-pointer hover:underline lg:text-[18px]'
+              onClick={() => navigate('/pokedex/hoenn')}>
+              Generation III{' '}
+              <span className='text-[15px] lg:-[16px] font-normal opacity-70'>{'(Hoenn)'}</span>
+            </p>
+            <p
+              className='font-bold hover:cursor-pointer hover:underline lg:text-[18px]'
+              onClick={() => navigate('/pokedex/sinnoh')}>
+              Generation IV{' '}
+              <span className='text-[15px] lg:-[16px] font-normal opacity-70'>{'(Sinnoh)'}</span>
+            </p>
+            <p
+              className='font-bold hover:cursor-pointer hover:underline lg:text-[18px]'
+              onClick={() => navigate('/pokedex/unova')}>
+              Generation V{' '}
+              <span className='text-[15px] lg:-[16px] font-normal opacity-70'>{'(Unova)'}</span>
+            </p>
+            <p
+              className='font-bold hover:cursor-pointer hover:underline lg:text-[18px]'
+              onClick={() => navigate('/pokedex/kalos')}>
+              Generation VI{' '}
+              <span className='text-[15px] lg:-[16px] font-normal opacity-70'>{'(Kalos)'}</span>
+            </p>
+            <p
+              className='font-bold hover:cursor-pointer hover:underline lg:text-[18px]'
+              onClick={() => navigate('/pokedex/alola')}>
+              Generation VII{' '}
+              <span className='text-[15px] lg:-[16px] font-normal opacity-70'>{'(Alola)'}</span>
             </p>
           </div>
         </div>
--- src/reuseables/formatRegionName.jsx
+++ src/reuseables/formatRegionName.jsx
@@ -1,4 +1,9 @@
 export const formatRegionName = (region) => {
   if (region === 'kanto') return 'kanto';
   if (region === 'johto') return 'original-johto';
+  if (region === 'hoenn') return 'hoenn';
+  if (region === 'sinnoh') return 'original-sinnoh';
+  if (region === 'unova') return 'original-unova';
+  if (region === 'kalos') return 'kalos-central';
+  if (region === 'alola') return 'original-alola';
 };
