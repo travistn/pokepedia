2015f2ce368e1c548d3d673c71747ba874e0e08b
travistn <travisn714@gmail.com>
2022-12-19 14:54:18 -0800

    Added titles for the pokedex page

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 import PokedexTable from '../components/PokedexTable';
 import { useGetPokeDexQuery } from '../redux/slices/pokemonApi';
-import { formatRegionName } from '../reuseables/formatRegionName';
+import { formatRegionName, formatRegionTitle } from '../reuseables/formatRegionName';
 
 const Pokedex = () => {
   const { pokeRegion } = useParams();
@@ -11,7 +11,10 @@ const Pokedex = () => {
 
   return (
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
-      <div className='w-11/12 lg:w-7/12 flex flex-col items-center'>
+      <div className='w-11/12 lg:w-7/12 flex flex-col items-center mt-8'>
+        <div>
+          <h1 className='font-bold text-[18px] lg:text-[24px]'>{formatRegionTitle(pokeRegion)}</h1>
+        </div>
         <div className='mt-8 rounded-md w-full bg-white p-4 lg:p-12'>
           <PokedexTable pokedex={pokedex?.pokemon_entries} />
         </div>
--- src/reuseables/formatRegionName.jsx
+++ src/reuseables/formatRegionName.jsx
@@ -7,3 +7,13 @@ export const formatRegionName = (region) => {
   if (region === 'kalos') return 'kalos-central';
   if (region === 'alola') return 'original-alola';
 };
+
+export const formatRegionTitle = (region) => {
+  if (region === 'kanto') return 'Kanto Pokédex (Generation I)';
+  if (region === 'johto') return 'Johto Pokédex (Generation II)';
+  if (region === 'hoenn') return 'Hoenn Pokédex (Generation III)';
+  if (region === 'sinnoh') return 'Sinnoh Pokédex (Generation IV)';
+  if (region === 'unova') return 'Unova Pokédex (Generation V)';
+  if (region === 'kalos') return 'Kalos Pokédex (Generation VI)';
+  if (region === 'alola') return 'Alola Pokédex (Generation VII)';
+};
