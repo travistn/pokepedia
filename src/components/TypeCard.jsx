c6fc63481436a55d58b854c1e0d8c96413797228
travistn <travisn714@gmail.com>
2022-12-19 16:13:38 -0800

    Clicking a type card will direct to corresponding type page

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const TypeCard = ({ type }) => {
+  const navigate = useNavigate();
+
   return (
     <div
       id={type?.name}
-      className={`w-[66px] lg:w-[84px] h-[26px] lg:h-[34px] flex justify-center items-center rounded-[4px] text-[12px] lg:text-[13px] uppercase bg-${type?.name} text-white cursor-pointer hover:opacity-80`}>
+      className={`w-[66px] lg:w-[84px] h-[26px] lg:h-[34px] flex justify-center items-center rounded-[4px] text-[12px] lg:text-[13px] uppercase bg-${type?.name} text-white cursor-pointer hover:opacity-80`}
+      onClick={() => navigate(`/type/${type?.name}`)}>
       {type?.name}
     </div>
   );
