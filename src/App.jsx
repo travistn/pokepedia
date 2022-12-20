2c84838f1440e056f485afbdb0851b0243c6495a
travistn <travisn714@gmail.com>
2022-12-19 16:09:00 -0800

    Added type route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Pokemon from './pages/Pokemon';
 import PokedexDirectory from './pages/PokedexDirectory';
 import Pokedex from './pages/Pokedex';
+import Type from './pages/Type';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/pokemon/:pokeId' element={<Pokemon />} />
         <Route path='/pokedex' element={<PokedexDirectory />} />
         <Route path='/pokedex/:pokeRegion' element={<Pokedex />} />
+        <Route path='/type/:typeId' element={<Type />} />
       </Routes>
     </>
   );
