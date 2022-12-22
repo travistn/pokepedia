b2ec4c6ab32b436294bab0c441e50721cee14f57
travistn <travisn714@gmail.com>
2022-12-22 15:15:55 -0800

    Created the ability directory page

---
+++ src/pages/AbilityDirectory.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AbilityDirectory = () => {
+  return (
+    <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
+      <div className='w-10/12 lg:w-7/12 mt-8 flex flex-col gap-8'>
+        <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
+          <h1 className='text-[24px] font-bold text-center'>Pokémon Abilities</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AbilityDirectory;
