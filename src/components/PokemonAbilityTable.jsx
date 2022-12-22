d20f8e8133ef1c3de95c9ca19b912b7340e14cf7
travistn <travisn714@gmail.com>
2022-12-22 14:53:51 -0800

    Added styling for desktop view

--- src/components/PokemonAbilityTable.jsx
+++ src/components/PokemonAbilityTable.jsx
@@ -10,32 +10,38 @@ const PokemonAbilityTable = ({ pokemon }) => {
     <table className='w-full'>
       <thead className='border-b'>
         <tr>
-          <th scope='col' className='text-sm font-bold text-gray-900 px-6 py-4 text-left'>
+          <th
+            scope='col'
+            className='text-[15px] lg:text-[17px] font-bold text-gray-900 px-6 py-4 text-left'>
             #
           </th>
-          <th scope='col' className='text-sm font-bold text-gray-900 px-6 py-4 text-left'>
+          <th
+            scope='col'
+            className='text-[15px] lg:text-[17px] font-bold text-gray-900 px-6 py-4 text-left'>
             Sprite
           </th>
-          <th scope='col' className='text-sm font-bold text-gray-900 px-6 py-4 text-left'>
+          <th
+            scope='col'
+            className='text-[15px] lg:text-[17px] font-bold text-gray-900 px-6 py-4 text-left'>
             Pokémon
           </th>
         </tr>
       </thead>
       <tbody>
         {pokemon?.map((poke) => (
-          <tr className='border-b'>
+          <tr className='border-b' key={poke?.pokemon.name}>
             <td
-              className='px-6 py-4 text-sm font-medium text-gray-900'
+              className='px-6 py-4 text-[15px] lg:text-[17px] text-gray-900 hover:cursor-pointer hover:underline'
               onClick={() => navigate(`/pokemon/${poke?.pokemon.name}`)}>
               {poke?.pokemon.url.substring(34).split('/').join('')}
             </td>
             <td>
-              <div className='h-[70px] w-[70px]'>
+              <div className='h-[70px] w-[70px] lg:h-[90px] lg:w-[90px]'>
                 <PokemonSprite pokemon={poke?.pokemon.name} />
               </div>
             </td>
             <td
-              className='text-[15px] capitalize text-[#3a80cd] font-bold px-6 py-4'
+              className='text-[15px] lg:text-[17px] capitalize text-[#3a80cd] font-bold px-6 py-4 hover:cursor-pointer hover:underline'
               onClick={() => navigate(`/pokemon/${poke?.pokemon.name}`)}>
               {poke?.pokemon.name.split('-').join(' ')}
             </td>
--- src/pages/Ability.jsx
+++ src/pages/Ability.jsx
@@ -14,34 +14,31 @@ const Ability = () => {
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
       <div className='w-10/12 lg:w-7/12 mt-8 flex flex-col gap-8'>
         <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
-          <h1 className='capitalize text-[24px] font-bold text-center'>
+          <h1 className='capitalize text-[24px] lg:text-[30px] font-bold text-center'>
             {ability?.name.split('-').join(' ')}
-            <span className='lowercase text-[20px] opacity-60'>{' (ability)'}</span>
+            <span className='lowercase text-[20px] lg:text-[24px] opacity-60'>{' (ability)'}</span>
           </h1>
           <div className='flex flex-col gap-2'>
-            <h1 className='text-[20px] font-bold'>Effect</h1>
-            <p className='text-[15px]'>{abilityEffect?.effect}</p>
+            <h1 className='text-[20px] lg:text-[26px] font-bold'>Effect</h1>
+            <p className='text-[15px] lg:text-[16px]'>{abilityEffect?.effect}</p>
           </div>
-          <div className='flex flex-col gap-12'>
-            <div className='flex flex-col gap-4'>
-              <h1 className='font-bold text-[20px]'>
-                Pokémon with{' '}
-                <span className='capitalize'>{ability?.name.split('-').join(' ')}</span>
-              </h1>
-              <PokemonAbilityTable
-                pokemon={ability?.pokemon.filter((ability) => ability?.is_hidden === false)}
-              />
-            </div>
-            <div className='flex flex-col gap-4'>
-              <h1 className='font-bold text-[20px] capitalize'>
-                {`${ability?.name.split('-').join(' ')} `}
-                <span className=''>as a hidden ability</span>
-              </h1>
-              <PokemonAbilityTable
-                pokemon={ability?.pokemon.filter((ability) => ability?.is_hidden === true)}
-              />
-            </div>
-          </div>
+        </div>
+        <div className='flex flex-col gap-4 bg-white rounded-md p-4'>
+          <h1 className='font-bold text-[20px] lg:text-[26px]'>
+            Pokémon with <span className='capitalize'>{ability?.name.split('-').join(' ')}</span>
+          </h1>
+          <PokemonAbilityTable
+            pokemon={ability?.pokemon.filter((ability) => ability?.is_hidden === false)}
+          />
+        </div>
+        <div className='flex flex-col gap-4 bg-white rounded-md p-4'>
+          <h1 className='font-bold text-[20px] lg:text-[26px] capitalize'>
+            {`${ability?.name.split('-').join(' ')} `}
+            <span className='lowercase'>as a hidden ability</span>
+          </h1>
+          <PokemonAbilityTable
+            pokemon={ability?.pokemon.filter((ability) => ability?.is_hidden === true)}
+          />
         </div>
       </div>
     </div>
