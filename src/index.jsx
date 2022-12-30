ad96675ed50d894db355221e64703ffd0fd7456b
travistn <travisn714@gmail.com>
2022-12-30 01:15:14 -0800

    Pages will be instantly scrolled to the top on route change

--- src/index.jsx
+++ src/index.jsx
@@ -6,11 +6,13 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { store } from './redux/store';
+import ScrollToTop from './helpers/ScrollToTop';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>
