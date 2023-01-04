56282da2ba8624d91ccc8f95d9d735c7c3ad6107
travistn <travisn714@gmail.com>
2023-01-03 22:31:05 -0800

    Added move introduction to the move data

--- src/components/MoveData.jsx
+++ src/components/MoveData.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
 import TypeCard from '../components/TypeCard';
+import { getMoveContact } from '../reuseables/getMoveContact';
+import { getMoveIntroduction } from '../reuseables/getMoveIntroduction';
 
 const MoveData = ({ move }) => {
-  const getMoveContact = (category) => {
-    if (category === 'special') return 'No';
-    if (category === 'physical') return 'Yes';
-    if (category === 'status') return 'N/A';
-  };
-
   return (
     <div className='flex flex-col gap-2'>
       <h1 className='font-bold text-[20px]'>Move Data</h1>
@@ -20,6 +16,7 @@ const MoveData = ({ move }) => {
           <p>Accuracy</p>
           <p>PP</p>
           <p>Makes contact?</p>
+          <p>Introduced</p>
         </div>
         <div className='flex flex-col gap-1'>
           <TypeCard type={move?.type} />
@@ -28,6 +25,7 @@ const MoveData = ({ move }) => {
           <p>{move?.accuracy !== null ? move?.accuracy : 'N/A'}</p>
           <p>{move?.pp}</p>
           <p>{getMoveContact(move?.damage_class.name)}</p>
+          <p>{getMoveIntroduction(move?.generation.name)}</p>
         </div>
       </div>
     </div>
