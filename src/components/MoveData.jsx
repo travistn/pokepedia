06e2d187352e97b764e8bad39e6f9be2dd427d8d
travistn <travisn714@gmail.com>
2023-01-03 16:24:28 -0800

    Created a move data table

--- src/components/MoveData.jsx
+++ src/components/MoveData.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import TypeCard from '../components/TypeCard';
 
 const MoveData = ({ move }) => {
+  const getMoveContact = (category) => {
+    if (category === 'special') return 'No';
+    if (category === 'physical') return 'Yes';
+    if (category === 'status') return 'N/A';
+  };
+
   return (
     <div className='flex flex-col gap-2'>
-      <h1 className='font-bold text-[18px]'>Move Data</h1>
-      <div className='flex flex-row gap-4'>
-        <div className='flex flex-col items-end'>
+      <h1 className='font-bold text-[20px]'>Move Data</h1>
+      <div className='flex flex-row gap-4 items-center'>
+        <div className='flex flex-col items-end opacity-70 gap-1'>
           <p>Type</p>
           <p>Category</p>
           <p>Power</p>
@@ -15,13 +21,13 @@ const MoveData = ({ move }) => {
           <p>PP</p>
           <p>Makes contact?</p>
         </div>
-        <div className='flex flex-col'>
+        <div className='flex flex-col gap-1'>
           <TypeCard type={move?.type} />
           <p className='capitalize'>{move?.damage_class.name}</p>
-          <p>{move?.power}</p>
+          <p>{move?.power !== null ? move?.power : 'N/A'}</p>
           <p>{move?.accuracy}</p>
           <p>{move?.pp}</p>
-          <p>{move?.damage_class.name === 'special' ? 'No' : 'Yes'}</p>
+          <p>{getMoveContact(move?.damage_class.name)}</p>
         </div>
       </div>
     </div>
