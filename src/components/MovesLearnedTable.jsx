2dd961769673b76371212dfacaea640ebb8b08fb
travistn <travisn714@gmail.com>
2023-01-05 23:51:52 -0800

    Added move types to the table

--- src/components/MovesLearnedTable.jsx
+++ src/components/MovesLearnedTable.jsx
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import TypeCard from './TypeCard';
 
 const MovesLearnedTable = ({ moves, method }) => {
   const [movesByMethod, setMovesByMethod] = useState([]);
+  const [movesData, setMovesData] = useState([]);
+
+  useEffect(() => {
+    const getMoves = async () => {
+      movesByMethod?.map(
+        async (move) =>
+          await axios
+            .get(`https://pokeapi.co/api/v2/move/${move.move.name}`)
+            .then((res) => setMovesData((current) => [...current, res.data]))
+      );
+    };
+    getMoves();
+  }, [movesByMethod]);
 
   useEffect(() => {
     setMovesByMethod(
@@ -11,19 +27,19 @@ const MovesLearnedTable = ({ moves, method }) => {
             .name === method
       )
     );
-  }, [method]);
+  }, [moves, method]);
 
   return (
     <div className='overflow-x-auto'>
       <table className='w-full'>
         <thead className='border-b'>
           <tr>
-            <th className='p-3 text-left'>Lv.</th>
-            <th className='p-3 text-left'>Move</th>
-            <th className='p-3 text-left'>Type</th>
-            <th className='p-3 text-left'>Cat.</th>
-            <th className='p-3 text-left'>Power</th>
-            <th className='p-3 text-left'>Acc.</th>
+            <th className='p-3 text-left text-[15px]'>Lv.</th>
+            <th className='p-3 text-left text-[15px]'>Move</th>
+            <th className='p-3 text-left text-[15px]'>Type</th>
+            <th className='p-3 text-left text-[15px]'>Cat.</th>
+            <th className='p-3 text-left text-[15px]'>Power</th>
+            <th className='p-3 text-left text-[15px]'>Acc.</th>
           </tr>
         </thead>
         <tbody>
@@ -34,15 +50,20 @@ const MovesLearnedTable = ({ moves, method }) => {
                 ? -1
                 : 1
             )
-            ?.map((move) => (
+            ?.map((move, index) => (
               <tr className='border-b'>
-                <td className='p-3 text-right'>
+                <td className='p-3 text-left text-[15px]'>
                   {
                     move.version_group_details[move.version_group_details.length - 1]
                       .level_learned_at
                   }
                 </td>
-                <td className='p-3 text-left'>{move?.move.name}</td>
+                <td className='p-3 text-left text-[15px] capitalize '>
+                  {move?.move.name.split('-').join(' ')}
+                </td>
+                <td>
+                  <TypeCard type={movesData[index]?.type} />
+                </td>
               </tr>
             ))}
         </tbody>
