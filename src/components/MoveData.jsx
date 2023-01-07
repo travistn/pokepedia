1480c78d58b4b051b39923cf681bbc299c8f27b7
travistn <travisn714@gmail.com>
2023-01-06 16:21:34 -0800

    Added styling for desktop view

--- src/components/MoveData.jsx
+++ src/components/MoveData.jsx
@@ -6,10 +6,10 @@ import { getMoveIntroduction } from '../reuseables/getMoveIntroduction';
 
 const MoveData = ({ move }) => {
   return (
-    <div className='flex flex-col gap-2'>
-      <h1 className='font-bold text-[20px]'>Move Data</h1>
+    <div className='flex flex-col gap-2 lg:gap-4'>
+      <h1 className='font-bold text-[20px] lg:text-[24px] lg:text-center'>Move Data</h1>
       <div className='flex flex-row gap-4 items-center'>
-        <div className='flex flex-col items-end opacity-70 gap-1'>
+        <div className='flex flex-col items-end opacity-70 gap-1 lg:gap-2'>
           <p>Type</p>
           <p>Category</p>
           <p>Power</p>
@@ -18,7 +18,7 @@ const MoveData = ({ move }) => {
           <p>Makes contact?</p>
           <p>Introduced</p>
         </div>
-        <div className='flex flex-col gap-1'>
+        <div className='flex flex-col gap-1 lg:gap-2'>
           <TypeCard type={move?.type} />
           <p className='capitalize'>{move?.damage_class.name}</p>
           <p>{move?.power !== null ? move?.power : 'N/A'}</p>
--- src/pages/Move.jsx
+++ src/pages/Move.jsx
@@ -13,19 +13,25 @@ const Move = () => {
 
   return (
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
-      <div className='w-10/12 lg:w-7/12 mt-8 flex flex-col gap-8'>
-        <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
-          <h1 className='capitalize text-center font-bold text-[24px]'>
+      <div className={`bg-${move?.type.name} w-10/12 lg:w-7/12 mt-8 flex flex-col p-4 rounded-md`}>
+        <div className='w-full flex flex-col gap-6 lg:gap-8 bg-white rounded-md p-4'>
+          <h1 className='capitalize text-center font-bold text-[24px] lg:text-[30px]'>
             {move?.name.split('-').join(' ')}
           </h1>
-          <MoveData move={move} />
-          <div className='flex flex-col gap-2'>
-            <h1 className='font-bold text-[20px]'>Effect</h1>
-            <p className='text-[15px]'>{move?.effect_entries[0].effect}</p>
-          </div>
-          <div className='flex flex-col gap-2'>
-            <h1 className='font-bold text-[20px]'>Move Target</h1>
-            <p className='text-[15px]'>{moveTarget?.description}</p>
+          <div className='flex flex-col lg:flex-row gap-6 w-full lg:justify-center'>
+            <div className='lg:w-full flex flex-col lg:items-center'>
+              <MoveData move={move} />
+            </div>
+            <div className='flex flex-col gap-6 lg:w-full'>
+              <div className='flex flex-col gap-2'>
+                <h1 className='font-bold text-[20px] lg:text-[24px]'>Effect</h1>
+                <p className='text-[15px] lg:text-[16px]'>{move?.effect_entries[0].effect}</p>
+              </div>
+              <div className='flex flex-col gap-2'>
+                <h1 className='font-bold text-[20px] lg:text-[24px]'>Move Target</h1>
+                <p className='text-[15px] lg:text-[16px]'>{moveTarget?.description}</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
