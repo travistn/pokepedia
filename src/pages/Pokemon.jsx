f4689bcb56a8b954298b4456399e6823d9feb7fe
travistn <travisn714@gmail.com>
2023-01-11 00:59:01 -0800

    No previous pokemon will show if it is the first registered pokemon

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -43,7 +43,11 @@ const Pokemon = () => {
             <div
               className='mr-auto flex flex-row items-center gap-2'
               onClick={() => navigate(`/pokemon/${previousPokemon?.pokemon_species.name}`)}>
-              <BiLeftArrowAlt className='lg:text-[18px] hover:cursor-pointer' />
+              {pokemon?.id !== 1 ? (
+                <BiLeftArrowAlt className='lg:text-[18px] hover:cursor-pointer' />
+              ) : (
+                ''
+              )}
               <p className='text-[15px] lg:text-[17px] capitalize hover:cursor-pointer hover:underline'>
                 {pokemon?.id !== 1
                   ? `#${previousPokemon?.entry_number} ${previousPokemon?.pokemon_species.name}`
