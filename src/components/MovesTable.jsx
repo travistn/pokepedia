02265efa8e05eddc16b0ebe4304f924197161c3f
travistn <travisn714@gmail.com>
2023-01-11 15:20:53 -0800

    Added moves table

---
+++ src/components/MovesTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MovesTable = ({ moves }) => {
+  return <div></div>;
+};
+
+export default MovesTable;
--- src/pages/Moves.jsx
+++ src/pages/Moves.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import MovesTable from '../components/MovesTable';
+import { useGetPokemonMovesQuery } from '../redux/slices/pokemonApi';
+
 const Moves = () => {
-  return <div></div>;
+  const { data: moves } = useGetPokemonMovesQuery();
+
+  return (
+    <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
+      <div className='w-10/12 lg:w-7/12 mt-8 flex flex-col gap-8'>
+        <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
+          <h1 className='font-bold text-[24px] text-center'>Pokémon Moves</h1>
+          <MovesTable moves={moves?.results} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Moves;
