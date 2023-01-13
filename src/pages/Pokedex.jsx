67cbf948c149d10d7d36e2272d98904ad899f94d
travistn <travisn714@gmail.com>
2023-01-13 14:15:15 -0800

    Added a loading spinner when page is loading

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import PokedexTable from '../components/PokedexTable';
+import Loading from '../components/Loading';
 import { useGetPokeDexQuery } from '../redux/slices/pokemonApi';
 import { formatRegionName, formatRegionTitle } from '../reuseables/formatRegionName';
 
 const Pokedex = () => {
   const { pokeRegion } = useParams();
-  const { data: pokedex } = useGetPokeDexQuery(formatRegionName(pokeRegion));
+  const { data: pokedex, isLoading } = useGetPokeDexQuery(formatRegionName(pokeRegion));
 
   return (
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
       <div className='w-11/12 lg:w-7/12 flex flex-col items-center mt-8'>
-        <div className='rounded-md w-full bg-white p-4 lg:p-12'>
-          <h1 className='font-bold text-[18px] lg:text-[26px] text-center mb-8'>
-            {formatRegionTitle(pokeRegion)}
-          </h1>
-          <PokedexTable pokedex={pokedex?.pokemon_entries} />
-        </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className='rounded-md w-full bg-white p-4 lg:p-12'>
+            <h1 className='font-bold text-[18px] lg:text-[26px] text-center mb-8'>
+              {formatRegionTitle(pokeRegion)}
+            </h1>
+            <PokedexTable pokedex={pokedex?.pokemon_entries} />
+          </div>
+        )}
       </div>
     </div>
   );
