3a4bc1b50a46cbfc4885994e7a7072013bd09d05
travistn <travisn714@gmail.com>
2023-01-13 14:08:17 -0800

    Added a loading spinner when page is loading

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 import ItemsTable from '../components/ItemsTable';
+import Loading from '../components/Loading';
 import { useGetPokemonItemsQuery } from '../redux/slices/pokemonApi';
 
 const Items = () => {
-  const { data: items } = useGetPokemonItemsQuery();
+  const { data: items, isLoading } = useGetPokemonItemsQuery();
 
   return (
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
       <div className='w-10/12 lg:w-7/12 mt-8 flex flex-col gap-8'>
-        <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
-          <h1 className='text-[24px] lg:text-[30px] font-bold text-center'>Pokémon Items</h1>
-          <ItemsTable items={items?.results} />
-        </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
+            <h1 className='text-[24px] lg:text-[30px] font-bold text-center'>Pokémon Items</h1>
+            <ItemsTable items={items?.results} />
+          </div>
+        )}
       </div>
     </div>
   );
