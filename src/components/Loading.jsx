66e8577bec5ce06ff1bdfe4f996676c0f9849fdd
travistn <travisn714@gmail.com>
2023-01-13 14:07:22 -0800

    first commit

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PacmanLoader } from 'react-spinners';
+
+const Loading = () => {
+  return (
+    <>
+      <PacmanLoader color='#ee6767' />
+    </>
+  );
+};
+
+export default Loading;
