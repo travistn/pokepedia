6854e00b24dbcce10f4fd9cdf2827778f3bd76f7
travistn <travisn714@gmail.com>
2023-01-13 14:09:54 -0800

    Added a loading spinner when page is loading

--- src/pages/Moves.jsx
+++ src/pages/Moves.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
 import MovesTable from '../components/MovesTable';
+import Loading from '../components/Loading';
 import { useGetPokemonMovesQuery } from '../redux/slices/pokemonApi';
 
 const Moves = () => {
-  const { data: moves } = useGetPokemonMovesQuery();
+  const { data: moves, isLoading } = useGetPokemonMovesQuery();
 
   return (
     <div className='min-h-screen flex flex-col items-center bg-sky-blue lg:bg-poke-bg bg-no-repeat bg-contain'>
       <div className='w-10/12 lg:w-7/12 mt-8 flex flex-col gap-8'>
-        <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
-          <h1 className='font-bold text-[24px] lg:text-[30px] text-center'>Pokémon Moves</h1>
-          <MovesTable moves={moves?.results} />
-        </div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className='w-full flex flex-col gap-6 bg-white rounded-md p-4'>
+            <h1 className='font-bold text-[24px] lg:text-[30px] text-center'>Pokémon Moves</h1>
+            <MovesTable moves={moves?.results} />
+          </div>
+        )}
       </div>
     </div>
   );
